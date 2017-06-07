a7b20a4150266c3be5a2a3ead3127e1036d65fc2
ludl3 <ludl3@yonyou.com>
2017-06-07 16:10:55 +0800

    修改webpack2的配置文件，和修改组件的版本

--- tank-battle/config.tank.battle.js
+++ tank-battle/config.tank.battle.js
@@ -3,11 +3,13 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
 var cwd = process.cwd();
 module.exports = {
-  entry: [
+  entry: {
+    app: [
     './webapp/main.js'
-  ],
+    ]
+  },
   output: {
-    path: path.join(__dirname, 'build'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/build/'//这里和server.js config.output.publicPath 对应是服务端产出路径
   },
@@ -15,54 +17,66 @@ module.exports = {
     port:8123
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js?$/,
         exclude: /node_modules/,
-        loaders: ['react-hot','babel-loader?presets[]=react,presets[]=es2015']
+        use: ['react-hot','babel-loader?presets[]=react,presets[]=es2015']
       },
       { 
         test: /\.less$/, 
-        loader: 'style-loader!css-loader!less-loader' 
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ] 
       }, 
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract("style-loader","css-loader")
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
       },
       { 
         test: /.(woff(2)?)(\?[a-z0-9=\.]+)?$/, 
-        loader: 'url?limit=10000&mimetype=application/font-woff' 
+        use: ['url-loader?limit=10000&mimetype=application/font-woff'] 
       },
       { 
         test: /.(eot)(\?[a-z0-9=\.]+)?$/, 
-        loader: 'file' 
+        use: ['file-loader'] 
       },
       { test: /.(ttf)(\?[a-z0-9=\.]+)?$/, 
-        loader: 'url?limit=10000&mimetype=application/octet-stream' 
+        use: ['url-loader?limit=10000&mimetype=application/octet-stream'] 
       },
       { 
         test: /.(svg)(\?[a-z0-9=\.]+)?$/, 
-        loader: 'url?limit=10000&mimetype=image/svg+xml' 
+        use: ['url-loader?limit=10000&mimetype=image/svg+xml' ]
       },
       { 
         test: /\.(png|jpg|woff|woff2|otf|eot|svg|ttf)$/, 
-        loader: 'url-loader?limit=8192' 
+        use: ['url-loader?limit=8192' ]
       } 
     ]
   },
   resolve: {
-    extension: ['', '.js', '.jsx', '.json'],
+    enforceExtension: true,
+    extensions: ['.js', '.jsx', '.json'],
     alias: {
-      "styles": path.join(cwd,"./webapp/styles"),
-      "less": path.join(cwd,"./webapp/styles/less"),
-      "constants": path.join(cwd, "./webapp/constants"),
-      "images": path.join(cwd, "./webapp/images"),
-      "pages": path.join(cwd, "./webapp/pages"),
-      "components": path.join(cwd, "./webapp/components"),
-      "utils": path.join(cwd, "./webapp/utils")
+      "styles": path.resolve(__dirname,"./webapp/styles"),
+      "less": path.resolve(__dirname,"./webapp/styles/less"),
+      "constants": path.resolve(__dirname, "./webapp/constants"),
+      "images": path.resolve(__dirname, "./webapp/images"),
+      "pages": path.resolve(__dirname, "./webapp/pages"),
+      "components": path.resolve(__dirname, "./webapp/components"),
+      "utils": path.resolve(__dirname, "./webapp/utils")
     }
   },
   plugins: [
-    new ExtractTextPlugin("styles.css")
+    new ExtractTextPlugin({
+      filename: "styles.css",
+      disable: false,
+      allChunks: true
+    })
   ]
 };
