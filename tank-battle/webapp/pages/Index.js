c3c0ea78d3b0b66a2bc223a27a9bd4e5d3d88c20
ludl3 <ludl3@yonyou.com>
2017-06-08 17:25:34 +0800

    初始化一个页面

--- tank-battle/webapp/pages/Index.js
+++ tank-battle/webapp/pages/Index.js
@@ -6,8 +6,9 @@ import React from 'react';
 import '../styles/css/index.css'
 class Index extends React.Component {
   render() {
+  	var marginLeftValue =(document.body.clientWidth-400)/2 ;
   	return(
-      <div></div>
+      <div className="index-page" style={{marginLeft:marginLeftValue}}></div>
   	);
   }
 }
