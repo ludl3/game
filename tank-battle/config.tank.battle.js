cc35215f7b0e5439740051f1a1a8389ea39b1674
zhaoyuu <zhaoyuu@yonyou.com>
2017-06-08 13:04:20 +0800

    修改配置文件

--- tank-battle/config.tank.battle.js
+++ tank-battle/config.tank.battle.js
@@ -3,9 +3,7 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
 var cwd = process.cwd();
 module.exports = {
-  entry: [
-    './webapp/main.js'
-  ],
+  entry: './webapp/main',
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
@@ -17,29 +15,19 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        use:[
-          {
-            loader:'react-hot-loader'
-          },
-          {
-            loader:'babel-loader',
-            options:{
-              presets:'react',
-              presets:'es2015'
-            }
-          }
-        ],
+        test: /\.js[x]?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['babel-loader'],
+        include: path.resolve(__dirname, 'webapp')
       },
-      { 
-        test: /\.less$/, 
+      {
+        test: /\.less$/,
         use: [
           'style-loader',
           'css-loader',
           'less-loader'
-        ] 
-      }, 
+        ]
+      },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
@@ -47,68 +35,58 @@ module.exports = {
           use: "css-loader"
         })
       },
-      { 
-        test: /.(woff(2)?)(\?[a-z0-9=\.]+)?$/, 
-        use: [
+      {
+        test: /.(woff(2)?)(\?[a-z0-9=\.]+)?$/,
+        use: {
           loader:'url-loader',
-          options:         
-          {
+          options:{
             limit:10000,
             mimetype:'application/font-woff'
-          }  
-        ] 
+          }
+        }
       },
-      { 
-        test: /.(eot)(\?[a-z0-9=\.]+)?$/, 
-        use: ['file-loader'] 
+      {
+        test: /.(eot)(\?[a-z0-9=\.]+)?$/,
+        use: ['file-loader']
       },
-      { test: /.(ttf)(\?[a-z0-9=\.]+)?$/, 
-        use: [
+      { test: /.(ttf)(\?[a-z0-9=\.]+)?$/,
+        use: {
           loader:'url-loader',
           options:
           {
             limit:10000,
             mimetype:'application/octet-stream'
           }
-        ] 
+        }
       },
-      { 
-        test: /.(svg)(\?[a-z0-9=\.]+)?$/, 
-        use: [
+      {
+        test: /.(svg)(\?[a-z0-9=\.]+)?$/,
+        use: {
           loader:'url-loader',
           options:
           {
             limit:10000,
             mimetype:'image/svg+xml'
           }
-        ] 
+        }
       },
-      { 
-        test: /\.(png|jpg|woff|woff2|otf|eot|svg|ttf)$/, 
-        use: [
+      {
+        test: /\.(png|jpg|woff|woff2|otf|eot|svg|ttf)$/,
+        use: {
           loader:'url-loader',
           options:
           {
             limit:8192
           }
-        ]
-      } 
+        }
+      }
     ]
   },
   resolve: {
-    enforceExtension: true,
-    extensions: ['.js', '.jsx', '.json'],
-    alias: {
-      "styles": path.join(cwd,"./webapp/styles"),
-      "less": path.join(cwd,"./webapp/styles/less"),
-      "constants": path.join(cwd, "./webapp/constants"),
-      "images": path.join(cwd, "./webapp/images"),
-      "pages": path.join(cwd, "./webapp/pages"),
-      "components": path.join(cwd, "./webapp/components"),
-      "utils": path.join(cwd, "./webapp/utils")
-    }
+    extensions: ['.js', '.jsx', '.json']
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new ExtractTextPlugin({
       filename: "styles.css",
       disable: false,
